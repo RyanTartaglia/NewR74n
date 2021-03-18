869061546d74891f6b8cc819bd404f866fb22fb1
Ryan <ryan@tartaglia.biz>
2021-03-17 20:36:25 -0400

    R74n

---
+++ convert/unitdata.js
@@ -0,0 +1,112 @@
+unitdata = {
+  /*
+  "category": {
+    "unit":["unit to base","base to unit"]
+  }
+  */
+  "data": { //base: bytes
+    "bit":0.125,
+    "nibble":0.5,
+    "byte":1,
+    "kilobyte":0.001, //1 byte to kilobytes
+    "kibibyte":1024,
+    "megabyte":1000000,
+    "mebibyte":1.049e+6,
+    "gigabyte":1000000000,
+    "gibibyte":1.074e+9,
+    "terabyte":1000000000000,
+    "tebibyte":1.1e+12,
+    "petabyte":1000000000000000,
+    "pebibyte":1.126e+15,
+    "exabyte": 1000000000000000000,
+    "exbibyte":1.153e+18,
+   "zettabyte":1000000000000000000000,
+    "zebibyte":9.223e+18,
+   "yottabyte":1000000000000000000000000,
+    "yobibyte":9.223e+18,
+    "kilobit":0.008,
+    "kibibit":0.0078125,
+    "megabit":8e-6,
+    "mebibit":131072,
+    "gigabit":8e-9,
+    "gibibit":1.342e+8,
+    "terabit":8e-12,
+    "tebibit":1.374e+11,
+    "petabit":8e-15,
+    "pebibit":1.407e+14,
+    "exabit":8e-18,
+    "exbibit":1.441e+17,
+    "zettabit":8e-21,
+    "zebibit":9.223e+18,
+    "yottabit":8e-24,
+    "yobibit":9.223e+18,
+    //35
+  },
+  "length": { //base: meter
+    "inch":0.0254,
+    "foot":0.3048,
+    "yard":0.9144,
+    "mile":1609.34,
+    "thou":2.54e-5,
+    "yoctometer":10**-24,
+    "zeptometer":10**-21,
+    "attometer":10**-18,
+    "femtometer":10**-15,
+    "picometer":10**-12,
+    "nanometer":10**-9,
+    "micrometer":10**-6,
+    "millimeter":10**-3,
+    "centimeter":10**-2,
+    "decimeter":10**-1,
+    "meter":1,
+    "decameter":10,
+    "hectometer":100,
+    "kilometer":1000,
+    "megameter":10**6,
+    "gigameter":10**9,
+    "terameter":10**12,
+    "petameter":10**15,
+    "exameter":10**18,
+    "zettameter":10**21,
+    "yottameter":10**24,
+    "ångström":1e-10,
+    "x unit":1e-13,
+    "scandinavian mile":10000,
+    "line":0.0021166666666666664,
+    "league":4800,
+    "R74meter":1.7526,
+    "fathom":1.8288,
+    "nautical mile":1852,
+    "chain":20.1168,
+    "rod":5.0292,
+    "earth radius":6371000,
+    "lunar distance":384402000,
+    "astronomical unit":149597870700,
+    "light year":9460730472580800,
+    "parsec":3.08567758146719e+16,
+    "gigaparsec":3.08567758146719e+25,
+    "planck length":1.6e-35,
+    "human hair thickness":8e-5,
+    "football field":110,
+
+
+
+
+  }
+};
+
+aliases = {
+  "thou":"mil",
+  "micrometer":"micron",
+  "rod":"pole / perch",
+  "meter":"metre",
+  "femtometer":"fermi",
+
+  
+}
+
+
+abbrs = {
+  "bit":"b","byte":"B","nibble":"nybble","kilobyte":"kB","kibibyte":"KiB","megabyte":"MB","mebibyte":"MiB","gigabyte":"GB","gibibyte":"GiB","terabyte":"TB","tebibyte":"TiB","petabyte":"PB","pebibyte":"PiB","exabyte":"EB","exbibyte":"EiB","zettabyte":"ZB","zebibyte":"ZiB","yottabyte":"YB","yobibyte":"YiB","kilobit":"kbit","kibibit":"Kibit","megabit":"Mbit","mebibit":"Mibit","gigabit":"Gbit","gibibit":"Gibit","terabit":"Tbit","tebibit":"Tibit","petabit":"Pbit","pebibit":"Pibit","exabit":"Ebit","exbibit":"Eibit","zettabit":"Zbit","zebibit":"Zibit","yottabit":"Ybit","yobibit":"Yibit","inch":"in","foot":"ft","yard":"yd","mile":"mi","yoctometer":"ym","zeptometer":"zm","attometer":"am","femtometer":"fm","picometer":"pm","nanometer":"nm","micrometer":"µm","millimeter":"mm","centimeter":"cm","decimeter":"dm","meter":"m","decameter":"dam","hectometer":"hm","kilometer":"km","megameter":"Mm","gigameter":"Gm","terameter":"Tm","petameter":"Pm","exameter":"Em","zettameter":"Zm","yottameter":"Ym","ångström":"Å","x unit":"xu","R74meter":"R74m","nautical mile":"M","earth radius":"R⊕","lunar distance":"LD","astronomical unit":"au","light year":"ly","parsec":"pc","gigaparsec":"Gpc","planck length":"𝓁P",
+
+}

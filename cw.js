28f8a3ea7046f39f6e71d558194afaabee2bc8cb
Ryan <ryan@tartaglia.biz>
2021-04-04 18:09:00 -0400

    R74n

--- cw.js
+++ cw.js
@@ -1,3 +1,4 @@
+window.addEventListener("load",function(){
 var elements = document.getElementsByClassName("cw");
 for (var i = 0; i < elements.length; i++) {
     var element = elements.item(i);
@@ -5,8 +6,9 @@ for (var i = 0; i < elements.length; i++) {
     var content = element.innerHTML;
     if (element.getAttribute("open")==null) {element.setAttribute("open","false")}
     if (element.getAttribute("open")=="false") {
-        element.setAttribute("onclick","this.innerHTML=\""+content.replace('"','\\"')+"\";this.setAttribute('open','true')")
+        element.setAttribute("onclick","this.innerHTML=`"+content.replace('"','\\"')+"`;this.setAttribute('open','true')")
         element.innerHTML = warning;
     }
     console.log(elements.item(i).getAttribute("open"));
 }
+})
